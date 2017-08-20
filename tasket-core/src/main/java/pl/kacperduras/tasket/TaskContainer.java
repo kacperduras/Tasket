d02eecb0d101221f8484ca43bbb13da9af2766bd
kacperduras <git@kacperduras.pl>
2017-08-20 16:35:31 +0200

    Add lifecycle logic.

--- tasket-core/src/main/java/pl/kacperduras/tasket/TaskContainer.java
+++ tasket-core/src/main/java/pl/kacperduras/tasket/TaskContainer.java
@@ -11,25 +11,70 @@ public class TaskContainer {
 
     private final String taskId;
     private final Task annotation;
+    private final Object instance;
     private final Method method;
 
     private Object id;
 
-    public TaskContainer(Tasket parent, Task annotation, Method method) {
+    public TaskContainer(Tasket parent, Task annotation, Object instance, Method method) {
         Validate.notNull(parent);
 
         Validate.notNull(annotation);
+        Validate.notNull(instance);
         Validate.notNull(method);
 
         this.parent = parent;
 
         this.taskId = annotation.id();
         this.annotation = annotation;
+        this.instance = instance;
         this.method = method;
     }
 
-    public Tasket getParent() {
-        return parent;
+    public boolean start(boolean force, Object... objects) {
+        Runnable runnable = () -> {
+            try {
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+
+                method.invoke(instance, objects);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+
+        if (id != null) {
+            boolean state = parent.state(id);
+            if (state && force) {
+                parent.stop(id);
+            } else {
+                return false;
+            }
+        }
+
+        id = parent.start(annotation, runnable);
+        if (id != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean start(Object... objects) {
+        return start(false, objects);
+    }
+
+    public boolean stop() {
+        if (id != null) {
+            boolean state = parent.state(id);
+            if (state) {
+                parent.stop(id);
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public String getTaskId() {
@@ -38,10 +83,6 @@ public class TaskContainer {
 
     public Task getAnnotation() {
         return annotation;
-    }
-
-    public Method getMethod() {
-        return method;
     }
 
 }
--- tasket-core/src/main/java/pl/kacperduras/tasket/Tasket.java
+++ tasket-core/src/main/java/pl/kacperduras/tasket/Tasket.java
@@ -1,8 +1,59 @@
 package pl.kacperduras.tasket;
 
+import org.apache.commons.lang3.Validate;
 import pl.kacperduras.tasket.annotation.Task;
 
+import java.lang.reflect.Method;
+import java.util.*;
+
 public abstract class Tasket {
+
+    private final Map<Class<?>, List<TaskContainer>> tasksMap = new HashMap<>();
+
+    public final Optional<TaskContainer> getTask(Class<?> parent, String id) {
+        Validate.notNull(parent);
+        Validate.notNull(id);
+
+        List<TaskContainer> containers = tasksMap.get(parent);
+        return containers.size() == 0 ? Optional.empty() : containers.stream()
+                .filter(task -> task.getTaskId().equals(id)).findAny();
+    }
+
+    public final Optional<TaskContainer> getTask(String id) {
+        Validate.notNull(id);
+
+        for (Class<?> aClass : tasksMap.keySet()) {
+            Optional<TaskContainer> optionalTask = getTask(aClass, id);
+            if (optionalTask.isPresent()) {
+                return optionalTask;
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public void loadTasks(Object instance) {
+        Validate.notNull(instance);
+
+        for (Method method : instance.getClass().getMethods()) {
+            Task task = method.getDeclaredAnnotation(Task.class);
+            if (task == null) {
+                continue;
+            }
+
+            String id = task.id();
+            if (getTask(id).isPresent()) {
+                throw new RuntimeException();
+            }
+
+            TaskContainer container = new TaskContainer(this, task, instance, method);
+            tasksMap.computeIfAbsent(instance.getClass(), map -> new ArrayList<>()).add(container);
+
+            if (task.timer().period() > 0) {
+                container.start(true);
+            }
+        }
+    }
 
     protected abstract Object start(Task annotation, Runnable runnable);
 
